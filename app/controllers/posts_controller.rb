6f801ac288185a0e23e3f14987b8e869dccb6625
haruka0410 <haruka.maruyama@di-v.co.jp>
2020-07-08 22:21:06 +0900

    second-commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
   def new
@@ -9,5 +9,18 @@ class PostsController < ApplicationController
   
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'posts#index'
   get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  get 'posts/:id', to: 'posts#checked'
   post 'posts', to: 'posts#create'
+ 
 end
